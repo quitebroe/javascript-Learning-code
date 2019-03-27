aba917d649fc224181ddd7a9c0ec9af7d5ca6c78
yyx219 <yyx219@qq.com>
2019-03-27 10:57:21 +0800

    remove test --all files

--- test/getPos.js
+++
@@ -1,6 +0,0 @@
-        function getPos(ev){
-        	var scrollTop = document.documentElement.scrollTop||document.body.scrollTop;
-        	var scrollLeft = document.documentElement.scrollLeft||document.body.scrollLeft; 
-            
-            return {x:ev.clientX+scrollLeft,y:ev.clientY+scrollTop};
-        }
